1b58f003cef3302fbf0dbea8bb50d8cff4bd40ec
Maid233 <74146511+Maid233@users.noreply.github.com>
2022-05-19 20:57:49 +0800

    简陋版实现

--- project/include/seam_carver.h
+++ project/include/seam_carver.h
@@ -11,13 +11,14 @@ class SeamCarver {
         }
         ~SeamCarver() {}
 
-        // 更新图像
+        // 实时更新图像
         void UpdateImg() {
             for(int i = 0; i < 100; ++i) {
                 CalculateEnergyMap(output_img);
-                FindSeam(energy_map, &seam);
+                FindSeam(energy_map);
                 RemoveSeam(seam);
 
+                // 实时显示找到的seam
                 cv::Mat test_image = output_img.clone();
                 for(int j = 0; j < output_img.rows; ++j)
                 {
@@ -29,6 +30,7 @@ class SeamCarver {
                 cv::waitKey(1);
             }
 
+            // 结果
             cv::imshow("output", output_img);
             cv::waitKey(0);
         }
@@ -49,23 +51,70 @@ class SeamCarver {
             cv::addWeighted(sobelX, 1, sobelY, 1, 0, energy_map);
         }
 
-        // 初始化map
-        void Initialize(const int rows, const int cols) {
-            map_data.clear();
-            map_path.clear();
-            map_data.resize(rows, std::vector<float>(cols, 0));
-            map_path.resize(rows, std::vector<int>(cols, 0));
-        }
-
         // 找到能量最少的一条seam
-        void FindSeam(const cv::Mat energy_map, std::vector<int> *seam) {
-            assert(seam);
-
+        void FindSeam(const cv::Mat energy_map) {
             const int rows = energy_map.rows;
             const int cols = energy_map.cols;
-            float upleft, upright, up, min_energy;
-            Initialize(rows, cols);
-            seam->resize(rows, 0);
+            int map_data[rows][cols];
+
+            int temp = 1919810;
+            int temp_index = -1;
+            seam.resize(rows, 0);
+
+            for(int i = 0; i < cols; ++i) {
+                map_data[0][i] = (int)energy_map.at<uchar>(0, i);
+            }
+
+            for(int i = 1; i < rows; ++i) {
+                for(int j = 0; j < cols; ++j) {
+                    // 边界检测
+                    if(j == 0)
+                        map_data[i][j] = std::min(map_data[i-1][j+1], map_data[i-1][j]);
+                    else if(j == cols-1)
+                        map_data[i][j] = std::min(map_data[i-1][j-1], map_data[i-1][j]);
+                    // 非边界情况
+                    else
+                        map_data[i][j] = std::min(map_data[i-1][j-1], std::min(map_data[i-1][j], map_data[i-1][j+1]));
+                    map_data[i][j] += energy_map.at<uchar>(i, j);
+                }
+            }
+
+            for(int i = 0; i < cols; ++i) {
+                // 寻找能量最低的起点
+                if(map_data[rows-1][i] < temp) {
+                    temp = map_data[rows-1][i];
+                    temp_index = i;
+                }
+            }
+
+            cv::Point p(rows-1, temp_index);
+            seam[p.x] = p.y;
+            for(int i = p.x; i > 0; --i) {
+                auto energy = map_data[i][p.y] - (int)energy_map.at<uchar>(i, p.y);
+                // 边界检测
+                if(p.y == 0) {
+                    if(energy == map_data[i-1][p.y+1])
+                        p = cv::Point(i-1, p.y+1);
+                    else
+                        p = cv::Point(i-1, p.y);
+                }
+                else if (p.y == cols - 1) {
+                    if (energy == map_data[i - 1][p.y - 1])
+                        p = cv::Point(i - 1, p.y - 1);
+                    else
+                        p = cv::Point(i - 1, p.y);
+                }
+                // 非边界情况
+                else {
+                    if (energy == map_data[i - 1][p.y - 1])
+                        p = cv::Point(i - 1, p.y - 1);
+                    else if (energy == map_data[i - 1][p.y + 1])
+                        p = cv::Point(i - 1, p.y + 1);
+                    else
+                        p = cv::Point(i - 1, p.y);
+                }
+                seam[i] = p.y;
+            }
         }
 
         // 删除Seam
@@ -76,15 +125,13 @@ class SeamCarver {
             const int cols = output_img.cols;
             for(int i = 0; i < rows; ++i) {
                 int temp = seam[i];
+                // 对于要移除的seam，左边的像素保持不动
                 for(int j = 0; j < temp; ++j) {
-                    new_img.at<cv::Vec3b>(i, j)[0] = output_img.at<cv::Vec3b>(i, j)[0];
-                    new_img.at<cv::Vec3b>(i, j)[1] = output_img.at<cv::Vec3b>(i, j)[1];
-                    new_img.at<cv::Vec3b>(i, j)[2] = output_img.at<cv::Vec3b>(i, j)[2];
+                    new_img.at<cv::Vec3b>(i, j) = output_img.at<cv::Vec3b>(i, j);
                 }
+                // 右边的像素向左缩进一格
                 for(int j = temp; j < cols - 1; ++j) {
-                    new_img.at<cv::Vec3b>(i, j)[0] = output_img.at<cv::Vec3b>(i, j + 1)[0];
-                    new_img.at<cv::Vec3b>(i, j)[1] = output_img.at<cv::Vec3b>(i, j + 1)[1];
-                    new_img.at<cv::Vec3b>(i, j)[2] = output_img.at<cv::Vec3b>(i, j + 1)[2];
+                    new_img.at<cv::Vec3b>(i, j) = output_img.at<cv::Vec3b>(i, j + 1);
                 }
             }
             output_img = new_img.clone();
@@ -103,7 +150,5 @@ class SeamCarver {
         cv::Mat energy_map;
         cv::Mat output_img;
 
-        std::vector<std::vector<float>> map_data;
-        std::vector<std::vector<int>> map_path;
         std::vector<int> seam;
 };
