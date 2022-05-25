fb1339c4c984a40ebc09140a92c9c8856bd4f3e6
Meidozuki <1025473107@qq.com>
2022-05-25 22:43:00 +0800

    Delete hard-code of img path

--- project/main.cpp
+++ project/main.cpp
@@ -5,11 +5,15 @@
 #include <memory>
 #include "seam_carver.h"
 
+#include "header_config.h"
+
 #define MOD 2
 
 int main(void) {
     // 记得使用绝对路径
-    std::string filename = "C:/Users/Hiyori/Documents/GitHub/SeamCarve2022Spring/project/test_image/HJoceanSmall.png";
+    std::string src_dir(SRC_DIR);
+    std::string filename = "/test_image/HJoceanSmall.png";
+    filename=src_dir + filename;
 
     SeamCarver sc(filename.c_str());
     sc.UpdateImg(MOD, 200);
