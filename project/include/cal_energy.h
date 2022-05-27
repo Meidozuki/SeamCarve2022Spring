11dc73b79e9a2681bf1fa1d4748652e013dd2aed
Meidozuki <1025473107@qq.com>
2022-05-27 13:29:23 +0800

    update cal_energy

--- project/include/cal_energy.h
+++ project/include/cal_energy.h
@@ -3,24 +3,29 @@
 //
 
 #pragma once
-#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <cstdint>
+
+typedef uint8_t dtype;
 
 void cal_gradient(const cv::Mat &gray_img,cv::Mat &dst,bool dx,bool dy) {
-//    const int cols=gray_img.cols,rows=gray_img.rows;
-//    dst=cv::Mat(rows,cols,gray_img.type());
-//
-//    if (dx) {
-//        for (int i=0;i < rows;++i) {
-//            for (int j=1;j < cols;++j) {
-//                dst.at<int>(i,j)=gray_img.at<int>(i,j)-gray_img.at<int>(i,j-1);
-//            }
-//        }
-//    }
-//    else if (dy) {
-//        for (int j=0;j < cols;++j) {
-//            for (int i=1;i < rows;++i) {
-//                dst.at<int>(i,j)=gray_img.at<int>(i,j)-dst.at<int>(i-1,j);
-//            }
-//        }
-//    }
+    const int cols=gray_img.cols,rows=gray_img.rows;
+    dst=gray_img.clone();
+
+    int i,j;
+    if (dx) {
+        for (i=0;i < rows;++i) {
+            for (j=1;j < cols;++j) {
+                auto dif=gray_img.at<dtype>(i,j)-gray_img.at<dtype>(i,j-1);
+                dst.at<dtype>(i,j)=dif;
+            }
+        }
+    }
+    else if (dy) {
+        for (j=0;j < cols;++j) {
+            for (i=1;i < rows;++i) {
+                dst.at<dtype>(i,j)=gray_img.at<dtype>(i,j)-dst.at<dtype>(i-1,j);
+            }
+        }
+    }
 }
